8df73aedac7420542fa505f292c1305808742d43
root <root@nicklinsky1.mylabserver.com>
2018-02-16 22:36:43 +0000

    Went back to old apache profile

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,28 +1,17 @@
-class profile::apache(
-  Boolean $default_vhost = false,
-  #Hash $apache_vhost_servers,
-  #String docroot = '/var/www/',
-  #Integer $port = 80,
-) {
-
-  #class {'::apache':
-  #  default_vhost => $default_vhost,
-  #}
-
-  #create_resources(::apache::vhost, $apache_vhost_servers)
-  #::apache::vhost{'homepages':
-  #  port => $port,
-  #  docroot => $docroot
-  #}
-
-  file {'/var/www':
-    ensure => directory,
+class profile::apache {
+  class { '::apache':
+    default_vhost => true,
   }
 
-  file {'/var/www/index.html':
-    ensure => file,
-    content => 'Test page. This is a test',
-    require => File['/var/www'],
+  $docroot = "/var/www/${facts['fqdn']}"
+
+  ::apache::vhost { $facts['fqdn']:
+    port    => 80,
+    docroot => $docroot,
+    before  => File["${docroot}/index.html"],
+  }
+
+  file {  "${docroot}/index.html":
+    content => "Test pageThis is a test",
   }
 }
-
